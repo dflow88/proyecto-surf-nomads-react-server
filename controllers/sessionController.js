bedd7a5daa5032b68cd83bee35529be17883a826
¨Diego <dieflope@gmail.com>
2021-08-20 09:24:16 -0500

    bug de Stripe, find through tour

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,16 +1,19 @@
-
+const Tour = require('../models/Tour');
+const { findById } = require('../models/User');
 
 exports.generateSession = async (req, res) => {
-
+    console.log("Entre a generatesession")
     const stripe = require('stripe')(process.env.STRIPE_KEY);
 
     const response = await req.body
+    const respuesta = await Tour.findById(req.body.tour)
+    const priceId = respuesta.stripeId
     console.log(response)
 
     const session = await stripe.checkout.sessions.create({
         line_items: [
             {
-                price: response.stripeId,
+                price: priceId,
                 quantity: response.totalDays
             },
         ],
@@ -20,10 +23,10 @@ exports.generateSession = async (req, res) => {
         ],
         success_url: `${process.env.REACT_URL}/payment-completed`,
         cancel_url: `${process.env.REACT_URL}/payment-cancelled`
-        })
-
-        res.json({
-            redirect_url: session.url
+    })
+    console.log(session)
+    res.json({
+        redirect_url: session.url
     })
 
 }
